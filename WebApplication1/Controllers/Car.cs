50f0f906f5d236e0a7fbdfb5a99a3a86cbdf3551
Sashagusew <rzhuludkov@bk.ru>
2022-05-21 15:35:35 +0300

    C#

---
+++ WebApplication1/Controllers/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class Car
+    {
+        public int id { get; set; }
+        public String image { get; set; }
+        public int price { get; set; }
+        public String brand { get; set; }
+        public String model { get; set; }
+        public String co2 { get; set; }
+        public String fuelCons { get; set; }
+
+        internal void Add(Car car)
+        {
+            throw new NotImplementedException();
+        }
+    };
+}
---
+++ WebApplication1/Controllers/Controler.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Controler : ControllerBase
+    {
+        List<Car> car;
+        [HttpGet("[action]")]
+        public Car get(int id)
+        {
+            return
+            car.FirstOrDefault(e => e.id == id);
+        }
+        [HttpPost("[action]")]
+        public void Add(Car car)
+        {
+            car.Add(car);
+        }
+
+
+
+    }
+}
